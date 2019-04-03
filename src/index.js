3884f76c89f0c5c7c6965b77330398510e8cc7f2
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-03 16:59:45 +0900

    Apollo Clientの設定

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloClient } from 'apollo-client';
+import {HttpLink, httpLink} from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import * as serviceWorker from './serviceWorker';
+const MESSAGE_APP_BASE_URL = 'https://nrgok-graphql-server-node-js.herokuapp.com/graphql';
+
+const httpLink = new HttpLink({
+    uri: MESSAGE_APP_BASE_URL,
+});
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+    link: httpLink,
+    cache,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
