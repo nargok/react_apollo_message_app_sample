ee736dc7077f5c6ca0ed0bbafad197e4f4efe69b
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-03 17:16:16 +0900

    Apollo Clientの設定2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import {HttpLink, httpLink} from 'apollo-link-http';
+import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from "apollo-cache-inmemory";
 
+// GraphQLで連携するサーバーのURL
 const MESSAGE_APP_BASE_URL = 'https://nrgok-graphql-server-node-js.herokuapp.com/graphql';
 
+// headerの設定などを担当するライブラリの設定
 const httpLink = new HttpLink({
     uri: MESSAGE_APP_BASE_URL,
 });
 
+// GraphQLの通信結果をキャッシュする設定
 const cache = new InMemoryCache();
+
+// ApolloClientの設定
 const client = new ApolloClient({
     link: httpLink,
     cache,
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ApolloClientをrootコンポーネントに適用する
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
