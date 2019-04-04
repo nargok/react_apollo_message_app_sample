03ab7b83aae6ae87f7667fbb3947fa6c336ac2dd
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-04 14:49:20 +0900

    Createコンポーネント:雛形を作成

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Mutatoin } from 'react-apollo';
 
-const Create = () => {
-    return (
-        <div>
-            <h1>メッセージ作成</h1>
-        </div>
-    )
+class Create extends Component {
+    constructor() {
+        super()
+        this.state = {
+            text: ""
+        }
+    }
+    render() {
+        const { text } = this.state;
+        return (
+            <div>
+                <h1>メッセージ作成</h1>
+                <div>
+                    <textarea
+                        name="message_text"
+                        id="message_text"
+                        cols="40"
+                        rows="10"
+                        placeholder="メッセージを入力してください"
+                        onChange={(event) => this.setState({ text: event.target.value})}
+                    >
+                    </textarea>
+                </div>
+                <button>投稿する</button>
+            </div>
+        )
+    }
 };
 
 export default Create;
