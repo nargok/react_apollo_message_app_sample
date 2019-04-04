e984048a6f93bd85ab634d1e3cafaff84402b0f9
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-04 11:15:18 +0900

    Loginコンポーネント:ユーザ名とパスワードをstateと連携

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-    return (
-        <div>
-            <h1>ログイン</h1>
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            loginName: "",
+            loginPassword: ""
+        }
+    }
+
+    handleChangeLoginName = (name) => {
+        this.setState({ loginName: name })
+    };
+
+    handleChangePassword = (password) => {
+        this.setState({ loginPassword: password })
+    };
+
+    render () {
+        return (
             <div>
-                <label htmlFor="login_name">ユーザ名</label>
-                <input type="text"　id="login_name" placeholder="ユーザ名"/>
+                <h1>ログイン</h1>
+                <div>
+                    <label htmlFor="login_name">ユーザ名</label>
+                    <input
+                        type="text"
+                        id="login_name"
+                        placeholder="ユーザ名"
+                        onChange={(event) => this.handleChangeLoginName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="login_name">パスワード</label>
+                    <input
+                        type="password"
+                        id="login_password"
+                        onChange={(event) => this.handleChangePassword(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <button>ログインする</button>
+                </div>
             </div>
-            <div>
-                <label htmlFor="login_name">パスワード</label>
-                <input type="password"　id="login_password" />
-            </div>
-            <div>
-                <button>ログインする</button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Login;
