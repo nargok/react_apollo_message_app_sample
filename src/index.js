8bef1154c8b6ec8d3f89262b475c86de8c882bfc
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-10 18:09:06 +0900

    試行錯誤中...一旦手を止める

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import Create from './components/Create';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from 'apollo-link';
+import { onError } from "apollo-link-error";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
 
@@ -33,14 +35,88 @@ const authLink = setContext((_, { headers }) => {
     }
 });
 
+// エラー設定
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+        for(let err of graphQLErrors) {
+            switch (err.extensions.code) {
+                case 'UNAUTHENTICATED':
+                    // ここにきたい
+                    console.log("えらーーー！！！");
+                    break;
+                default:
+            }
+        }
+    }
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 // GraphQLの通信結果をキャッシュする設定
 const cache = new InMemoryCache();
 
 // ApolloClientの設定
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    // link: authLink.concat(httpLink),
+    link: ApolloLink.from([authLink.concat(httpLink), errorLink]),
     cache,
 });
+
+// onError: ({ graphQLErrors, networkError, operaiton, forward }) => {
+//     if (graphQLErrors) {
+//         for(let err of graphQLErrors) {
+//             switch (err.extensions.code) {
+//                 case 'UNAUTHENTICATED':
+//                     console.log('ログインが必要なんだよ(´・ω・｀)');
+//                     // トークンをリフレッシュする
+//                     // const newToken = getNewToken();
+//                     // const headers = operaiton.getContext().headers;
+//                     // operaiton.setContext({
+//                     //     ...headers,
+//                     //     "x-token": newToken
+//                     // })
+//                     break;
+//                 default:
+//             }
+//         }
+//     }
+// }
+
+
+
+// コピーここから
+// const client = new ApolloClient({
+//     uri: '<your graphql endpoint>',
+//     // Apollo Boost allows you to specify a custom error link for your client
+//     onError: ({ graphQLErrors, networkError, operation, forward }) => {
+//         if (graphQLErrors) {
+//             for (let err of graphQLErrors) {
+//                 // handle errors differently based on its error code
+//                 switch (err.extensions.code) {
+//                     case 'UNAUTHENTICATED':
+//                         // old token has expired throwing AuthenticationError,
+//                         // one way to handle is to obtain a new token and
+//                         // add it to the operation context
+//                         const headers = operation.getContext().headers
+//                         operation.setContext({
+//                             headers: {
+//                                 ...headers,
+//                                 authorization: getNewToken(),
+//                             },
+//                         });
+//                         // Now, pass the modified operation to the next link
+//                         // in the chain. This effectively intercepts the old
+//                         // failed request, and retries it with a new token
+//                         return forward(operation);
+//
+//                     // handle other errors
+//                     case 'ANOTHER_ERROR_CODE':
+//                     // ...
+//                 }
+//             }
+//         }
+//     },
+// });
+// コピーここまで
 
 // ApolloClientをrootコンポーネントに適用する
 ReactDOM.render(
