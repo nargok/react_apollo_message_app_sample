fa842757f1ea62a3906d659cc248d314303afc9d
nargok <nargok@gmail.com>
2019-04-20 13:49:46 +0900

    message listing with admin user information

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+
+const PERSONAL_MESSAGE_LIST = gql`
+  {
+    user(id: 1) {
+      username
+      messages {
+        id
+        text
+      }
+    }
+  }
+`;
 
 class PersonalPage extends Component {
   constructor() {
@@ -11,7 +26,28 @@ class PersonalPage extends Component {
   render() {
     return (
       <div>
-        <h1>XXXXさんのメッセージ一覧</h1>
+        <h1>Message App</h1>
+        <Query query={PERSONAL_MESSAGE_LIST}>
+        {({ loading, error, data }) => {
+          if (loading) { return <div>Loading...</div> }
+          else if (error) { return <div>error...</div> }
+          else {
+            console.log(data.user);
+            const { messages } = data.user;
+            return (
+              <div>
+                <h2>{data.user.username}さんのメッセージ一覧</h2>
+                <ul>
+                  { messages.map(message => {
+                      return <li key={ message.id }>{ message.text }</li>
+                    })
+                  }
+                </ul>
+              </div>
+            )
+          }
+        }}
+        </Query>
       </div>
     )
   }
