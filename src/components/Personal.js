c1b591da65d2ddec284b5f0f4227d3a424117ce5
nargok <nargok@gmail.com>
2019-04-20 13:26:29 +0900

    add personal component

---
+++ src/components/Personal.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PersonalPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>XXXXさんのメッセージ一覧</h1>
+      </div>
+    )
+  }
+}
+
+export default PersonalPage;
