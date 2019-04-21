64cc1d5c7176e8fe6a03e46bcb8acf2ed5bdae62
nargok <nargok@gmail.com>
2019-04-21 13:57:14 +0900

    style for login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,52 @@ import { Mutation } from 'react-apollo';
 import { AUTH_TOKEN } from '../constants';
 import MenuAppBar from "./MenuAppBar";
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    dense: {
+        marginTop: 16,
+    },
+    menu: {
+        width: 200,
+    },
+});
+
+class OutlinedTextFields extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: this.props,
+        };
+    }
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+          <form noValidate autoComplete="off">
+              <TextField
+                id="outlined-name"
+                label={this.props.name}
+                value={this.state.name}
+                onChange={(e) => this.props.changeField(e.target.value)}
+                type={this.props.type}
+                margin="normal"
+                variant="outlined"
+              />
+          </form>
+
+        )
+    }
+}
 
 const LOGIN = gql`
   mutation signIn($loginName: String!, $loginPassword: String!){
@@ -37,23 +83,16 @@ class Login extends Component {
             <div>
                 <MenuAppBar />
                 <h1>ログイン</h1>
-                <div>
-                    <label htmlFor="login_name">ユーザ名</label>
-                    <input
-                        type="text"
-                        id="login_name"
-                        placeholder="ユーザ名"
-                        onChange={(event) => this.handleChangeLoginName(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="login_name">パスワード</label>
-                    <input
-                        type="password"
-                        id="login_password"
-                        onChange={(event) => this.handleChangePassword(event.target.value)}
-                    />
-                </div>
+                <OutlinedTextFields
+                  name="Name"
+                  type="text"
+                  changeField={this.handleChangeLoginName}
+                />
+                <OutlinedTextFields
+                  name="Password"
+                  type="password"
+                  changeField={this.handleChangePassword}
+                />
                 <div>
                     <Mutation
                         mutation={LOGIN}
